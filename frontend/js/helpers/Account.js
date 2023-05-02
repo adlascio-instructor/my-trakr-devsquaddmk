64f6cbb232bed714a76fc17635c12647e28358c7
Kevin Lee <kevinlfcola@gmail.com>
2023-05-02 11:13:07 -0700

    display the information

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -4,14 +4,36 @@ $(document).ready(() => {
         url: "http://localhost:3000/accounts",
         dataType: "json",
     }).done((data) => {
-        // console.log("data", data);
+        console.log("data", data);
         $.each(data, function (indexInArray, accounts) {
             console.log("Accounts", accounts);
-            // console.log("ArrayTransactions", accounts.transactions);
             $.each(
                 accounts.transactions,
                 function (indexInArray, transactions) {
                     console.log("Transactions", transactions);
+                    if (transactions.type === "transfer") {
+                        $("#transInformation").append(`<tr>
+                        <td>${transactions.id}</td>
+                        <td>${accounts.username}</td>
+                        <td>${transactions.type}</td>
+                        <td>"Category"</td>
+                        <td>${transactions.description}</td>
+                        <td>${transactions.amount}</td>
+                        <td>${transactions.accountIdFrom}</td>
+                        <td>${transactions.accountIdTo}</td
+                        </tr>`);
+                    } else {
+                        $("#transInformation").append(`<tr>
+                        <td>${transactions.id}</td>
+                        <td>${accounts.username}</td>
+                        <td>${transactions.type}</td>
+                        <td>"Category"</td>
+                        <td>${transactions.description}</td>
+                        <td>${transactions.amount}</td>
+                        <td>N/A</td>
+                        <td>N/A</td
+                        </tr>`);
+                    }
                 }
             );
         });
