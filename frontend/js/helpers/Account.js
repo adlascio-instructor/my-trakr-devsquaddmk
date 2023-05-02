ab8d83cf7ee77bbb214aadda1189c2de6f6b3e76
Kevin Lee <kevinlfcola@gmail.com>
2023-05-02 08:51:19 -0700

    got account information

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -4,6 +4,16 @@ $(document).ready(() => {
         url: "http://localhost:3000/accounts",
         dataType: "json",
     }).done((data) => {
-        console.log(data);
+        // console.log("data", data);
+        $.each(data, function (indexInArray, accounts) {
+            console.log("Accounts", accounts);
+            // console.log("ArrayTransactions", accounts.transactions);
+            $.each(
+                accounts.transactions,
+                function (indexInArray, transactions) {
+                    console.log("Transactions", transactions);
+                }
+            );
+        });
     });
 });
