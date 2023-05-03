5ebb0ddd020665ea6b28c1e93a64c3f9151bfa3c
Moizes42 <moizesfilho2@gmail.com>
2023-05-03 10:16:23 -0700

    Adding the balance to the accounts

--- frontend/js/helpers/Account.js
+++ frontend/js/helpers/Account.js
@@ -2,25 +2,28 @@
     
    $(document).ready(() => {
 
+    //Getting the Accounts from the server and showing them on the Accounts Summary
     $.ajax({
         method: "get",
         url: "http://localhost:3000/accounts",
     }).done((accounts) => {
         //console.log("accounts", accounts);
         $.each(accounts, (index, account) => {
-            console.log("account", account);
+            //console.log("account", account);
 
             $(".summaryAccounts").children("ul").append(`
             <li>
               <h3>${account.username}</h3>
+              <h3 class="balance"></h3>
             </li>`);
         })
     });
 
 
+    //Posting the new Accounts on the server
     $("#accountForm").submit(event => {
         
-        console.log("submit");
+        //console.log("submit");
         const inputAccount = $(".inputAccount").val();
         $.ajax({
             method: "post",
@@ -30,7 +33,7 @@
             url: "http://localhost:3000/accounts",
             dataType: "json",
         }).done((data) => {
-            console.log("accounnt data", data);
+            console.log("account data", data);
         })
     })
         
